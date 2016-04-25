14e4b2cbf2f861f7bcf6d33ea585cce61e4d6f9b
Will Breaden Madden <zicodian@gmail.com>
2016-04-25 12:30:46 +0200

    update setup.py version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def main():
 
     setuptools.setup(
         name             = "datavision",
-        version          = "2016.04.20.1528",
+        version          = "2016.04.25.1027",
         description      = "Python data visualisation",
         long_description = Markdown_to_reStructuredText("README.md"),
         url              = "https://github.com/wdbm/datavision",
